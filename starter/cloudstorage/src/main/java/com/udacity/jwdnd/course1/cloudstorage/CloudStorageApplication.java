add020e4ec0d4c4293e8d5dd1abcc1cb3f0c970f
YasmineMaheeb <yasminemaheeb@gmail.com>
2020-08-11 00:08:21 +0200

    ControllerAdvice annotation

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -2,26 +2,9 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 
 import com.udacity.jwdnd.course1.cloudstorage.config.StorageProperties;
-import com.udacity.jwdnd.course1.cloudstorage.services.StorageService;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
-import org.apache.coyote.http11.AbstractHttp11Protocol;
-//import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-//import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-
 
 @EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
@@ -31,70 +14,4 @@ public class CloudStorageApplication {
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
 
-//	@Bean
-//	public MultipartResolver multipartResolver() {
-//		CommonsMultipartResolver multipartResolver
-//				= new CommonsMultipartResolver();
-//		multipartResolver.setMaxUploadSize(5242880);
-//		return multipartResolver;
-//	}
-
-
-//	@Bean
-//	public TomcatServletWebServerFactory tomcatEmbedded() {
-//
-//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-//
-//		tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-//
-//			// connector other settings...
-//
-//			// configure maxSwallowSize
-//			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
-//				// -1 means unlimited, accept bytes
-//				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
-//			}
-//
-//		});
-//
-//		return tomcat;
-//
-//	}
-
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(redirectConnector());
-				tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-
-			// connector other settings...
-
-			// configure maxSwallowSize
-			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
-				// -1 means unlimited, accept bytes
-				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
-			}
-
-		});
-		return tomcat;
-	}
-
-	private Connector redirectConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
-	}
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileUploadExceptionAdvice.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileUploadExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
-import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@ControllerAdvice
 public class FileUploadExceptionAdvice {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
@@ -21,14 +23,4 @@ public class FileUploadExceptionAdvice {
         return modelAndView;
     }
 
-    @ExceptionHandler(FileSizeLimitExceededException.class)
-    public ModelAndView handleFileSizeException(
-            MaxUploadSizeExceededException exc,
-            HttpServletRequest request,
-            HttpServletResponse response) {
-
-        ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.getModel().put("message", "File too large!");
-        return modelAndView;
-    }
 }
